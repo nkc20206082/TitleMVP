634d054957727cd7fee6e5c78aff760286a27e15
nkc20206082 <79066907+nkc20206082@users.noreply.github.com>
2022-03-18 21:25:31 +0300

    Add files via upload

--- InputEvent.cs
+++ InputEvent.cs
@@ -13,23 +13,22 @@ public class InputEvent : MonoBehaviour
     bool _getkeyflg = false;
     bool _selectinterval = false;
     int _Phasenum = 0;
-    const int _PhaseChangenum= 1;
-    const int _PhaseSoundnum= 10;
+    const int _PhaseChangenum = 1;
 
     //縦操作方向
     public enum VerOperation : int
     {
-        UP=1,
-        DOWN=-1,
-        DEFAULT=0
+        UP = 1,
+        DOWN = -1,
+        DEFAULT = 0
     }
-    
+
     //横操作方向
     public enum HorOperation : int
     {
-        RIGHT=1,
-        LEFT=-1,
-        DEFAULT=0
+        RIGHT = 1,
+        LEFT = -1,
+        DEFAULT = 0
     }
 
     //タイトル画面内の遷移
@@ -38,9 +37,7 @@ public class InputEvent : MonoBehaviour
         TITLEPHASE = 0,
         OPTIONPHASE = 1,
         SOUNDPHASE = 2,
-        CREDITPHASE = 3,
-        BGMPHASE= SOUNDPHASE + _PhaseSoundnum,
-        SEPHASE= 1+SOUNDPHASE + _PhaseSoundnum
+        CREDITPHASE = 3
     }
 
     //タイトル画面の要素
@@ -63,7 +60,7 @@ public class InputEvent : MonoBehaviour
     {
         BGM = 0,
         SE = 1,
-        RETURN=2
+        RETURN = 2
     }
 
     void Update()
@@ -84,12 +81,6 @@ public class InputEvent : MonoBehaviour
                     break;
                 case (int)Phase.CREDITPHASE:
                     CreditSelect();
-                    break;
-                case (int)Phase.BGMPHASE:
-                    BGMOption();
-                    break;
-                case (int)Phase.SEPHASE:
-                    SEOption();
                     break;
             }
         }
@@ -127,7 +118,7 @@ public class InputEvent : MonoBehaviour
                 case (int)TitleMenu.START:
                     break;
                 case (int)TitleMenu.OPTION:
-                    _Phasenum+= _PhaseChangenum;
+                    _Phasenum += _PhaseChangenum;
                     break;
                 case (int)TitleMenu.CLOSE:
                     break;
@@ -170,10 +161,10 @@ public class InputEvent : MonoBehaviour
                     StartCoroutine("SelectInterval");
                     break;
                 case (int)OptionMenu.VOLUME:
-                    _Phasenum+= (int)OptionMenu.VOLUME;
+                    _Phasenum += (int)OptionMenu.VOLUME;
                     break;
                 case (int)OptionMenu.CREDIT:
-                    _Phasenum +=  (int)OptionMenu.CREDIT;
+                    _Phasenum += (int)OptionMenu.CREDIT;
                     break;
             }
         }
@@ -182,38 +173,75 @@ public class InputEvent : MonoBehaviour
     void SoundMenuSelect()
     {
         float VerSelect = Input.GetAxisRaw("Vertical");
-        switch (VerSelect)
+        float HorSelect = Input.GetAxisRaw("Horizontal");
+
+        if (HorSelect != 0)
         {
-            case (int)VerOperation.UP:
-                if (!_getkeyflg)
-                {
-                    _SoundMenumodel.GoBack();
-                }
-                KeyDown();
-                break;
-            case (int)VerOperation.DOWN:
-                if (!_getkeyflg)
-                {
-                    _SoundMenumodel.GoNext();
-                }
-                KeyDown();
-                break;
-            case (int)VerOperation.DEFAULT:
-                KeyUp();
-                break;
+            switch (HorSelect)
+            {
+                case (int)HorOperation.RIGHT:
+                    if (!_getkeyflg)
+                    {
+                        switch (_SoundMenumodel.DecisionMenu())
+                        {
+                            case (int)SoundMenu.BGM:
+                                _BGMmodel.BGMVoluemeUp();
+                                break;
+                            case (int)SoundMenu.SE:
+                                _SEmodel.SEVoluemeUp();
+                                break;
+                        }
+                    }
+                    KeyDown();
+                    break;
+                case (int)HorOperation.LEFT:
+                    if (!_getkeyflg)
+                    {
+                        switch (_SoundMenumodel.DecisionMenu())
+                        {
+                            case (int)SoundMenu.BGM:
+                                _BGMmodel.BGMVoluemeDown();
+                                break;
+                            case (int)SoundMenu.SE:
+                                _SEmodel.SEVoluemeDown();
+                                break;
+                        }
+                    }
+                    KeyDown();
+                    break;
+                case (int)HorOperation.DEFAULT:
+                    KeyUp();
+                    break;
+            }
+        }
+        else
+        {
+            switch (VerSelect)
+            {
+                case (int)VerOperation.UP:
+                    if (!_getkeyflg)
+                    {
+                        _SoundMenumodel.GoBack();
+                    }
+                    KeyDown();
+                    break;
+                case (int)VerOperation.DOWN:
+                    if (!_getkeyflg)
+                    {
+                        _SoundMenumodel.GoNext();
+                    }
+                    KeyDown();
+                    break;
+                case (int)VerOperation.DEFAULT:
+                    KeyUp();
+                    break;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Debug.Log(_Soundmodel.DecisionMenu());
             switch (_SoundMenumodel.DecisionMenu())
             {
-                case (int)SoundMenu.BGM:
-                    _Phasenum +=  _PhaseSoundnum+(int)SoundMenu.BGM;
-                    break;
-                case (int)SoundMenu.SE:
-                    _Phasenum += _PhaseSoundnum  + (int)SoundMenu.SE;
-                    break;
                 case (int)SoundMenu.RETURN:
                     _Phasenum -= _PhaseChangenum;
                     break;
@@ -231,67 +259,6 @@ public class InputEvent : MonoBehaviour
         }
     }
 
-    //BGMの操作
-    void BGMOption()
-    {
-        float HorSelect = Input.GetAxisRaw("Horizontal");
-        switch (HorSelect)
-        {
-            case (int)HorOperation.RIGHT:
-                if (!_getkeyflg)
-                {
-                    _BGMmodel.BGMVoluemeUp();
-                }
-                KeyDown();
-                break;
-            case (int)HorOperation.LEFT:
-                if (!_getkeyflg)
-                {
-                    _BGMmodel.BGMVoluemeDown();
-                }
-                KeyDown();
-                break;
-            case (int)HorOperation.DEFAULT:
-                KeyUp();
-                break;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _Phasenum -= _PhaseSoundnum+(int)SoundMenu.BGM;
-        }
-    }
-
-    //SEの操作
-    void SEOption()
-    {
-        float HorSelect = Input.GetAxisRaw("Horizontal");
-        switch (HorSelect)
-        {
-            case (int)HorOperation.RIGHT:
-                if (!_getkeyflg)
-                {
-                    _SEmodel.SEVoluemeUp();
-                }
-                KeyDown();
-                break;
-            case (int)HorOperation.LEFT:
-                if (!_getkeyflg)
-                {
-                    _SEmodel.SEVoluemeDown();
-                }
-                KeyDown();
-                break;
-            case (int)HorOperation.DEFAULT:
-                KeyUp();
-                break;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _Phasenum -= _PhaseSoundnum  + (int)SoundMenu.SE;
-        }
-    }
 
     //キーを押している
     void KeyDown()
